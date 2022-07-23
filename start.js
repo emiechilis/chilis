0c29a13bcd28b7cadb2f198440bdcdceb4b205d9
Emie Chilis <102459913+emiechilis@users.noreply.github.com>
2022-07-23 07:26:55 +0200

    Update start.js

--- start.js
+++ start.js
@@ -35,7 +35,7 @@
 		/*** End Firebase ***/
 		
 		/*** Styler ***/
-		layer(a){return go.h($(`.loader`), a)},
+		layer(a){return $(`.loader`).innerHTML = a},
 		metatag(a){
 			$(`head`).append(this.nod(`<meta ${a.view}></meta>`));
 			$(`body`).innerHTML = a.xml();
